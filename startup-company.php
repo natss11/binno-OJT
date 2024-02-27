32d3a545ba058c0af9179089beb21b3da79692f9
natss11 <marrianejaneonate.natad@gmail.com>
2024-02-28 01:31:57 +0800

    company gen

--- startup-company.php
+++ startup-company.php
@@ -81,7 +81,7 @@ if (!$companies) {
             </p>
 
             <!-- Cards Section -->
-            <div class="grid grid-cols-1 md:grid-cols-4 gap-6 mb-10 mx-20">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-10 mx-20">
                 <?php
                 $i = 1;
                 foreach ($companies as $company) {
